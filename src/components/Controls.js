855f17adee477eceb803883ab37e1a3a0d71db5e
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2021-12-27 14:07:29 +0200

    add style

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,18 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import s from "./Feedback.module.css";
 
 export default function Controls({ options, onLeaveFeedback }) {
    const btnMap = Object.keys(options);
     return (
-<div>
+     <div>
     {btnMap.map((item, ind) => (
         <button
         key={Math.random(3)}
         name={item}
         type="button"
         onClick={onLeaveFeedback}
+        className={s.btn}
         >
-            {["Добре","Нейтрально","Погано"][ind]}
+            {["Good","Neutral","Bad"][ind]}
         </button>
     ))}
 </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import s from "./Feedback.module.css"
 
 export default function Section({ title , children }) {
     return (
-        <div>
+        <div className={s.containerHeading}>
           {<h2>{title}</h2>}
           {children}
        </div>
